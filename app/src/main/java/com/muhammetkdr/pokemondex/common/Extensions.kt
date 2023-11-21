19e19d59c0fd7be943762b8d8132918b821312f7
Muhammet KDR <mskdr1453@gmail.com>
2023-11-21 18:29:12 +0300

    now its working

--- app/src/main/java/com/muhammetkdr/pokemondex/common/Extensions.kt
+++ app/src/main/java/com/muhammetkdr/pokemondex/common/Extensions.kt
@@ -5,9 +5,47 @@ import com.bumptech.glide.Glide
 import com.muhammetkdr.pokemondex.R
 
 
-fun ImageView.setPokemonImage(pokePath:String?) {
-    Glide.with(this).load(pokePath)
-        .placeholder(R.drawable.ic_downloading)
-        .error(R.drawable.ic_error)
-        .into(this)
+fun ImageView.setPokemonImage(pokePath: String?) {
+    Glide.with(this).load(pokePath).placeholder(R.drawable.ic_downloading)
+        .error(R.drawable.ic_error).into(this)
+}
+
+fun getPokemonColorByType(pokemonType: String): Int = when (pokemonType) {
+    "bug" -> R.color.background_type_bug
+
+    "dark" -> R.color.background_type_dark
+
+    "dragon" -> R.color.background_type_dragon
+
+    "electric" -> R.color.background_type_electric
+
+    "fairy" -> R.color.background_type_fairy
+
+    "fighting" -> R.color.background_type_fighting
+
+    "fire" -> R.color.background_type_fire
+
+    "flying" -> R.color.background_type_flying
+
+    "ghost" -> R.color.background_type_ghost
+
+    "grass" -> R.color.background_type_grass
+
+    "ground" -> R.color.background_type_ground
+
+    "ice" -> R.color.background_type_ice
+
+    "normal" -> R.color.background_type_normal
+
+    "poison" -> R.color.background_type_poison
+
+    "psychic" -> R.color.background_type_psychic
+
+    "rock" -> R.color.background_type_rock
+
+    "steel" -> R.color.background_type_steel
+
+    "water" -> R.color.background_type_water
+
+    else -> R.color.background_type_normal
 }
--- app/src/main/java/com/muhammetkdr/pokemondex/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/muhammetkdr/pokemondex/ui/detail/DetailFragment.kt
@@ -1,5 +1,6 @@
 package com.muhammetkdr.pokemondex.ui.detail
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.muhammetkdr.pokemondex.common.NetworkResponse
+import com.muhammetkdr.pokemondex.common.getPokemonColorByType
 import com.muhammetkdr.pokemondex.common.setPokemonImage
 import com.muhammetkdr.pokemondex.databinding.FragmentDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -50,50 +52,60 @@ class DetailFragment @Inject constructor() : Fragment() {
 
                 is NetworkResponse.Success -> {
                     binding.ivPokemon.setPokemonImage(args.imgUrl)
+                    var text = ""
 
-//                    binding.tvDescr.text = response.data.name
+                    response.data.types?.forEach { text += it.type?.name + " " }  // TODO TYPELARI BURADA RV icine bascaz
+
+                    binding.tvDescr.text = text
+
+                    val type = response.data.types?.first()?.type?.name ?: ""
+                    val color = getPokemonColorByType(type)
+//                    binding.statsContainer.statsConstraintL.setBackgroundColor(requireContext().getColor(color))
 
                     binding.tvWeight.text = response.data.weight.toString()
                     binding.tvHeight.text = response.data.height.toString()
                     binding.tvMoves.text = response.data.moves?.first()?.move?.name.toString()
 
-                        val progress = response.data.stats?.get(0)?.baseStat
-/*                        binding.progressBar.max = 200
-                        binding.progressBar.setProgress(progress!!,true)
-                        binding.progressBar.progressTintList = ColorStateList.valueOf(requireContext().getColor(R.color.blue))*/
+                    with(binding.statsContainer) {
 
-                    with(binding.statsContainer){
                         tvHPValue.text = response.data.stats?.first { it.stat?.name == "hp" }?.baseStat.toString()
                         tvAttackValue.text = response.data.stats?.first { it.stat?.name == "attack" }?.baseStat.toString()
                         tvDefValue.text = response.data.stats?.first { it.stat?.name == "defense" }?.baseStat.toString()
                         tvSpAttackValue.text = response.data.stats?.first { it.stat?.name == "special-attack" }?.baseStat.toString()
                         tvSpDefenseValue.text = response.data.stats?.first { it.stat?.name == "special-defense" }?.baseStat.toString()
                         tvSpeedValue.text = response.data.stats?.first { it.stat?.name == "speed" }?.baseStat.toString()
-                    }
 
-                    with(binding.statsContainer){
-                       pbHP.setProgress(response.data.stats?.first { it.stat?.name == "hp" }?.baseStat!!,true)
-                       pbAttack.setProgress( response.data.stats?.first { it.stat?.name == "attack" }?.baseStat!!,true)
-                       pbDef.setProgress( response.data.stats?.first { it.stat?.name == "defense" }?.baseStat!!,true)
-                       pbSpAtk.setProgress( response.data.stats?.first { it.stat?.name == "special-attack" }?.baseStat!!,true)
-                       pbSpDef.setProgress( response.data.stats?.first { it.stat?.name == "special-defense" }?.baseStat!!,true)
-                       pbSpeed.setProgress( response.data.stats?.first { it.stat?.name == "speed" }?.baseStat!!,true)
-                    }
+                        pbHP.setProgress(response.data.stats?.first { it.stat?.name == "hp" }?.baseStat!!, true)
+                        pbAttack.setProgress(response.data.stats?.first { it.stat?.name == "attack" }?.baseStat!!, true)
+                        pbDef.setProgress(response.data.stats?.first { it.stat?.name == "defense" }?.baseStat!!, true)
+                        pbSpAtk.setProgress(response.data.stats?.first { it.stat?.name == "special-attack" }?.baseStat!!, true)
+                        pbSpDef.setProgress(response.data.stats?.first { it.stat?.name == "special-defense" }?.baseStat!!, true)
+                        pbSpeed.setProgress(response.data.stats?.first { it.stat?.name == "speed" }?.baseStat!!, true)
 
+                        pbHP.progressTintList = ColorStateList.valueOf(requireContext().getColor(color))
+                        pbAttack.progressTintList = ColorStateList.valueOf(requireContext().getColor(color))
+                        pbDef.progressTintList = ColorStateList.valueOf(requireContext().getColor(color))
+                        pbSpAtk.progressTintList = ColorStateList.valueOf(requireContext().getColor(color))
+                        pbSpDef.progressTintList = ColorStateList.valueOf(requireContext().getColor(color))
+                        pbSpeed.progressTintList = ColorStateList.valueOf(requireContext().getColor(color))
+                    }
                 }
             }
         }
 
-        viewModel.pokemonStateSpecies.observe(viewLifecycleOwner){response->
-            when(response){
+        viewModel.pokemonStateSpecies.observe(viewLifecycleOwner) { response ->
+            when (response) {
                 is NetworkResponse.Error -> {
 
                 }
+
                 NetworkResponse.Loading -> {
 
                 }
+
                 is NetworkResponse.Success -> {
-                    binding.tvDescr.text = response.data.flavorTextEntries?.get(9)?.flavorText
+                    // TODO BURASI description olacak!!
+//                    binding.tvDescr.text = response.data.flavorTextEntries?.get(9)?.flavorText
                 }
             }
         }
